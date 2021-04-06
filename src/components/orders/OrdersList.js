d3d41839486c8cd13dcc7d3132652e651fafefa7
Alejandro Silva <a.s.cebreros@gmail.com>
2021-04-05 20:59:43 -0500

    detalles finales

--- src/components/orders/OrdersList.js
+++ src/components/orders/OrdersList.js
@@ -40,7 +40,7 @@ export default function OrdersList() {
                     Pedidos realizados 
                 </h3>
             </div>
-            {orders ?
+            {orders.length > 0 ?
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
